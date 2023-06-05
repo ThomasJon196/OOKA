665bba7c63cd59914f345806efc8702a25403fbc
Thomas Jonas <thomas.jonas@inovex.de>
2023-06-05 11:13:11 +0200

    Fixed analyzing controller

--- WirSchiffenDas/analyzer-component/src/main/java/com/example/analyzercomponent/AnalyzingController.java
+++ WirSchiffenDas/analyzer-component/src/main/java/com/example/analyzercomponent/AnalyzingController.java
@@ -1,5 +1,10 @@
 package com.example.analyzercomponent;
 
+
+import java.io.*;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import reactor.core.publisher.Mono;
 import java.time.*;
 import java.time.temporal.ChronoUnit;
@@ -26,28 +31,34 @@ public class AnalyzingController {
 	}
 
 	@PostMapping("/configuration/test")
-	public String executeAlgorithm(@RequestParam("configItem1") String configItem1,
+	public void executeAlgorithm(@RequestParam("configItem1") String configItem1,
 			@RequestParam("configItem2") String configItem2,
 			@RequestParam("configItem3") String configItem3, @RequestParam("configItem4") String configItem4) {
 
 		final String url = "http://localhost:8081/fuel/optional_equipment";
 		RestTemplate restTemplate = new RestTemplate();
+		// restTemplate.postForObject(url, "test", String.class);
 
-		Map<String, Object> params = new HashMap<>(4);
-		params.put("configItem1", configItem1);
-		params.put("configItem2", configItem2);
-		params.put("configItem3", configItem3);
-		params.put("configItem4", configItem4);
-		String result = restTemplate.getForObject(url, String.class, params);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-		System.out.println(result);
+		MultiValueMap<String, String> configuration = new LinkedMultiValueMap<>();
+		configuration.add("configItem1", configItem1);
+		configuration.add("configItem2", configItem2);
+		configuration.add("configItem3", configItem3);
+		configuration.add("configItem4", configItem4);
 
-		return result;
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(configuration, headers);
+		try {
+			ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+		} catch (Exception e) {
+			System.out.println("Invokation failed. Service unreachable.");
+		}
 	}
 
 	@GetMapping("/get_status")
 	public Mono<String> get_event() {
-		
+
 		Mono<String> result = component_access.read_component_status();
 		// String res = result.
 
